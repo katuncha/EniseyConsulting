f79ab7b9568fa39b4155cce280f954bcf60f082d
katuncha <demkamark@gmail.com>
2022-05-04 23:14:32 +0300

    add skroll edits end form Php

--- js/script.js
+++ js/script.js
@@ -22,7 +22,44 @@ const swiper = new Swiper('.swiper', {
 
   watchOverflow: true,
 
-  spaceBetween: 20,
+  slidesPerGroup: 2,
+
+  speed: 600,
+
+  breakpoints: {
+    320: {
+      slidesPerGroup: 1,
+    },
+    1024: {
+      slidesPerGroup: 2,
+    }
+  },
+
+});
+
+const swiperReviews = new Swiper('.review__swiper', {
+  navigation: {
+    nextEl: '.arrows__item--next',
+    prevEl: '.arrows__item--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+
+  simulateTouch: true,
+  grabCursor: true,
+
+  // mousewheel: {
+  //   sensitivity: 1,
+  //   eventsTarget: ".swiper-slide"
+  // },
+
+  slidesPerView: "auto",
+
+  watchOverflow: true,
 
   slidesPerGroup: 2,
 
@@ -37,6 +74,5 @@ const swiper = new Swiper('.swiper', {
     }
   },
 
-
-
 });
+
