7dcca1516d4eeb3da1b59d57d95b8230816e4471
katuncha <demkamark@gmail.com>
2022-05-06 00:12:38 +0300

    add cookie

--- js/script.js
+++ js/script.js
@@ -40,3 +40,28 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+//set cookie
+
+const cookies_box = document.getElementById('cookies_box'),
+cookies_button = document.getElementById('cookies_button');
+
+cookies_button.addEventListener('click',function(){
+  document.cookie = "CookieBy=InventionTricks; expires="+ new Date(2023, 0, 1).toUTCString();
+  document.cookie = "Name=John; max-age="+60*60*24*30;
+  document.cookie = "LastName=Deo; max-age="+60*60*24*30;
+
+  if(document.cookie){
+    cookies_box.classList.add('hide');
+  }
+})
+
+function getCookieName(name){
+  var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+  return r ? r[1]:'';
+}
+
+var getCookieName = getCookieName('CookieBy');
+
+if(getCookieName == 'InventionTricks') {
+  cookies_box.classList.add('hide');
+}
